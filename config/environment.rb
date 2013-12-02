93db85a5c202e9453cd10ef750286a474e42ab45
centuryboy0410 <mixcdmarket@gmail.com>
2013-12-02 21:47:31 +0900

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,5 @@ CLIENT_ID = 'b6d97e95e1f04ee38264a630b10fa5d3'
 CRON_TOKEN = ''
 
 # Target tags ['shiba', 'shibainu', 'shibadog', 'shibas']
-TARGET_TAGS = []
+TARGET_TAGS = [beautygirlscollection]
 
